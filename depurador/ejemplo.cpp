75d6efcbe8e6abd4da644c2a469393da768cefc8
Mariano <mbelgrano@gmail.com>
2024-05-28 13:26:45 -0300

    depurador y guias 6 y 7

---
+++ depurador/ejemplo.cpp
@@ -0,0 +1,22 @@
+
+// Ejercicio TP Ej
+// Autor:
+// Fecha:
+// Comentario
+
+# include<iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+    int ingreso = 10;
+    for (int i=0; i < ingreso; i++) {
+        num = ingreso * i ;
+        cout << num << endl;
+
+
+        }
+
+	return 0;
+}
