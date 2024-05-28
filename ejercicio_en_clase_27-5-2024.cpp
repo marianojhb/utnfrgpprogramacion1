853bf01005d32efc1b7cf788fb86309700455632
Mariano <mbelgrano@gmail.com>
2024-05-28 14:32:51 -0300

    ejercicio en clase 27-5-2024

--- ejercicio_en_clase_27-5-2024.cpp
+++ ejercicio_en_clase_27-5-2024.cpp
@@ -7,79 +7,198 @@
 
 using namespace std;
 
+// FUNCIONES:
 
-void cargarVector(int v[], int cant){
-    for (int i=0; i<cant;i++) {
+// 1) Carga el vector en sistema
+void cargarVector(int v[], int cant) {
+    for (int i=0; i<cant; i++) {
         cout << "INGRESE NUM #" << i+1 << ": ";
         cin >> v[i];
     }
 }
 
+// 2) Calcula el promedio de un vector
 float calcularPromedio(int v[], int cant) {
     int promedio;
-    int suma=0;
-    for (int i=0; i<cant; i++)
-    {
-        suma += v[i];
+    int acumulador=0;
+    for (int i=0; i<cant; i++) {
+        acumulador += v[i];
     }
-    promedio = (float)suma/cant;
+    promedio = (float)acumulador/cant;
     return promedio;
 }
 
+// (A) Cuenta los numeros mayores a un valor de comparación dado
 int cantMayores(int v[], int cant, int valorComparacion) {
-    int cantMayores=0;
-    cout << "cantMayores=" << cantMayores << endl;
+    int contador=0;
     for (int i=0; i<cant; i++) {
-        if (v[i]>valorComparacion)
-        {
-            cantMayores++;
-            cout << "cantMayores=" << cantMayores << endl;
+        if (v[i]>valorComparacion) {
+            contador++;
         }
     }
-    return cantMayores;
+    return contador;
 }
 
-int main()
-{
-    const int TAM=3;
-    int valorComparacion;
-    int promedio;
-    int respuesta;
+// (B) Cuenta los numeros menores a un valor de comparación dado
+int cantMenores(int v[], int cant, int valorComparacion) {
+    int contador=0;
+    for (int i=0; i<cant; i++) {
+        if (v[i]<valorComparacion) {
+            contador++;
+        }
+    }
+    return contador;
+}
+
+// (C) Cuenta los numeros iguales a un valor de comparación dado
+int cantIgual(int v[], int cant, int valorComparacion) {
+    int contador=0;
+    for (int i=0; i<cant; i++) {
+        if (v[i]==valorComparacion) {
+            contador++;
+        }
+    }
+    return contador;
+}
+
+// (D) Busca el máximo
+int maximo (int v[], int cant) {
+    int maximo;
+    for (int i=0; i<cant; i++) {
+        if (i==0) {
+            maximo=v[i];
+        } else {
+            if(v[i] > maximo) {
+                maximo=v[i];
+            }
+        }
+
+    }
+    return maximo;
+}
+
+// (E) Busca el minimo
+int minimo (int v[], int cant) {
+    int minimo;
+    for (int i=0; i<cant; i++) {
+        if (i==0) {
+            minimo=v[i];
+        } else {
+            if(v[i] < minimo) {
+                minimo=v[i];
+            }
+        }
+
+    }
+    return minimo;
+}
+
+// (F) Busca la cantidad que se repite el maximo
+int repeticionesMaximo (int v[], int cant) {
+    int maximo;
+    int repeticionesMaximo=0;
+    for (int i=0; i<cant; i++) {
+        if (i==0) {
+            maximo=v[i];
+        } else {
+            if(v[i] > maximo) {
+                maximo=v[i];
+            } else if (v[i]== maximo) {
+                repeticionesMaximo++;
+            }
+        }
+
+    }
+    return repeticionesMaximo;
+}
+
+// (G) Busca la cantidad que se repite el maximo
+int repeticionesMinimo (int v[], int cant) {
+    int minimo;
+    int repeticionesMinimo=0;
+    for (int i=0; i<cant; i++) {
+        if (i==0) {
+            minimo=v[i];
+        } else {
+            if(v[i] < minimo) {
+                minimo=v[i];
+            } else if (v[i]== minimo) {
+                repeticionesMinimo++;
+            }
+        }
+
+    }
+    return repeticionesMinimo;
+}
 
 
-    // vector
+// FUNCION MAIN
+int main() {
+    // TAMAÑO DEL VECTOR
+    int TAM;
 
-    int vEdad[TAM];
+    // MENU
     int menu=0;
-    cout << "INGRESAR VALOR COMPARACION " << endl;
+    cout << "INGRESAR DIMENSION DEL VECTOR: ";
+    cin >> TAM;
+    system("cls");
+    cout << "INGRESAR COMPARADOR:" << endl;
     cout << "1. PROMEDIO" << endl;
     cout << "2. INGRESO MANUAL" << endl;
-    cin >> menu;
-
-    // ingresar los datos
-    cout << "INGRESAR LAS EDADES: " << endl;
-    cargarVector(vEdad,TAM);
     cout << endl;
+    cin >> menu;
+    system("cls");
 
-
+    // VARIABLES NECESARIAS
+    int valorComparacion;
+    int respuesta;
+    int vEdad[TAM];
 
     switch (menu) {
-        case 1:
-            promedio=calcularPromedio(vEdad, TAM);
-            valorComparacion=promedio;
-            respuesta=cantMayores(vEdad, TAM, valorComparacion);
-            cout << "CANTIDAD DE ALUMNOS CON EDADES MAYORES AL PROMEDIO " << respuesta;
-            break;
 
-        case 2:
-            cout << "INGRESE VALOR A COMPARAR: ";
-            cin >> valorComparacion;
-            respuesta = cantMayores(vEdad, TAM, valorComparacion);
-            cout << "CANTIDAD DE ALUMNOS CON EDADES MAYORES AL INGRESO " << respuesta;
-            break;
+    case 1:
+        // INGRESO DE DATOS
+        cout << "INGRESAR LAS EDADES: " << endl;
+        cargarVector(vEdad,TAM);
+        cout << endl;
+        valorComparacion = calcularPromedio(vEdad, TAM);
+        respuesta = cantMayores(vEdad, TAM, valorComparacion);
+        cout << "(1) CANTIDAD DE ALUMNOS CON EDADES MAYORES AL PROMEDIO: " << respuesta << endl;
+        cout << endl;
+        break;
+
+    case 2:
+        // INGRESO MANUAL DE VALOR A COMPARAR
+        cout << "INGRESE VALOR A COMPARAR: ";
+        cin >> valorComparacion;
+        cout << endl;
+        // INGRESO DE DATOS
+        cout << "INGRESAR LAS EDADES: " << endl;
+        cargarVector(vEdad,TAM);
+        cout << endl;
+        // RESPUESTA A
+        respuesta = cantMayores(vEdad, TAM, valorComparacion);
+        cout << "(A) CANTIDAD DE ALUMNOS CON EDADES MAYORES AL VALOR INGRESADO " << respuesta << endl;
+        // RESPUESTA B
+        respuesta = cantMenores(vEdad, TAM, valorComparacion);
+        cout << "(B) CANTIDAD DE ALUMNOS CON EDADES MENORES AL VALOR INGRESADO " << respuesta << endl;
+        // RESPUESTA C
+        respuesta = cantIgual(vEdad, TAM, valorComparacion);
+        cout << "(C) CANTIDAD DE ALUMNOS CON EDADES IGUAL AL VALOR INGRESADO " << respuesta << endl;
+        cout << endl;
+        break;
     }
-
-
-
+    respuesta = maximo(vEdad, TAM);
+    cout << "(D) VALOR MAXIMO INGRESADO " << respuesta << endl;
+    cout << endl;
+    respuesta = minimo(vEdad, TAM);
+    cout << "(E) VALOR MINIMO INGRESADO " << respuesta << endl;
+    cout << endl;
+    respuesta = repeticionesMaximo(vEdad, TAM);
+    cout << "(F) REPETICIONES DEL VALOR MAXIMO " << respuesta << endl;
+    cout << endl;
+    respuesta = repeticionesMinimo(vEdad, TAM);
+    cout << "(G) REPETICIONES DEL VALOR MINIMO " << respuesta << endl;
+    cout << endl;
     return 0;
 }
